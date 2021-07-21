23450f12dedb2f1c28410fcbba731b945b60e1d1
yahir91 <yahir.cardona@gmail.com>
2021-07-21 13:53:04 -0600

    routing and redirect

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
+const fs = require('fs');
 const http = require('http')
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
-    res.setHeader('Content-Type ')
+    // set header content type
+    res.setHeader('Content-Type', 'text/html')
+
+    let path = './view/'
+    switch(req.url) {
+        case '/':
+            path += 'index.html'
+            res.statusCode = 200
+            break
+        case '/about':
+            path += 'about.html'
+            res.statusCode = 200
+            break
+        case '/about-me':
+            // path += 'about.html'
+            res.statusCode = 301
+            res.setHeader('Location', '/about')
+            res.end()
+            break
+        default: 
+        path +=  '404.html'
+        res.statusCode = 404
+        break    
+    }
+    console.log(req.url)
+    // send a thml file
+    fs.readFile(path, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end()
+        } else {
+            // res.write(data);
+            res.end(data)
+        }
+    })
+
 });
 
 server.listen(3000, 'localhost', () => {
