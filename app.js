4284b68848ccb8379869756d6f676f3f248f635c
yahir91 <yahir.cardona@gmail.com>
2021-08-01 00:34:40 -0600

    Code for creating and accessing database collections

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 
 // express app
@@ -22,21 +23,38 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
-
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('in the next middleware');
-  next();
-});
-
 app.use(morgan('dev'));
+
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  });
+  blog.save().then((result)=> {
+    res.send(result)
+  }).catch((err) => {
+    console.log(err)
+  })
+})
+
+app.get('/all-blogs', (req, res)=> {
+  Blog.find()
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((err)=> {
+    console.log(err)
+  })
+})
+
+app.get('/single-blog', (req, res) =>{
+  Blog.findById('61063de02a42a3293da9ca36')
+  .then((result) => {
+    res.send(result)
+  })
+})
 
 app.use((req, res, next) => {
   res.locals.path = req.path;
